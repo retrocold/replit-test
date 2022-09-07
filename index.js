0fb2f4d832dc84651057f329f6118a2798510acf
Retrocold <90989264+retrocold@users.noreply.github.com>
2022-09-07 13:34:39 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const CPU = require("os").cpus();
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "OK",
+    cores: CPU.length + " cores",
+    cpus: CPU[0],
+    env: process.env.SOME_ENV || "none",
+  });
+});
+
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), async function () {
+  console.log(`Server running on port ${app.get("port")}`);
+});
